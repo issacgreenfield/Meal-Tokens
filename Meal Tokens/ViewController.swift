674ed3b9e7c080e0da7ebb8b67db8fd0bca90c1c
IssacGreenfield <issacgreenfield@gmail.com>
2015-06-17 12:52:25 -0700

    added "!" to make this fail, testing how version control works

--- Meal Tokens/ViewController.swift
+++ Meal Tokens/ViewController.swift
@@ -9,6 +9,124 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private var tokenCalculator: Int? = 0
+    
+    
+    @IBOutlet weak var mealTokens: UILabel!
+    
+    @IBOutlet weak var snackTokens: UILabel!
+    
+    
+    @IBAction func workout15Button(sender!: UIButton)
+    {
+        var digit: Int = sender.currentTitle!.toInt()!
+
+        switch digit
+        {
+        case 15:
+            tokenCalculator = tokenCalculator! + 1
+        case 30:
+            tokenCalculator = tokenCalculator! + 2
+        case 45:
+            tokenCalculator = tokenCalculator! + 3
+        case 60:
+            tokenCalculator = tokenCalculator! + 4
+        default:
+            break
+        }
+        
+        mealTokens.text = String(tokenCalculator! / 3)
+        snackTokens.text = String(tokenCalculator! % 3)
+    }
+    @IBAction func workout30Button(sender: UIButton)
+    {
+        var digit: Int = sender.currentTitle!.toInt()!
+        
+        switch digit
+        {
+        case 15:
+            tokenCalculator = tokenCalculator! + 1
+        case 30:
+            tokenCalculator = tokenCalculator! + 2
+        case 45:
+            tokenCalculator = tokenCalculator! + 3
+        case 60:
+            tokenCalculator = tokenCalculator! + 4
+        default:
+            break
+        }
+        
+        mealTokens.text = String(tokenCalculator! / 3)
+        snackTokens.text = String(tokenCalculator! % 3)
+
+    }
+    
+    @IBAction func workout45Button(sender: UIButton)
+    {
+        var digit: Int = sender.currentTitle!.toInt()!
+        
+        switch digit
+        {
+        case 15:
+            tokenCalculator = tokenCalculator! + 1
+        case 30:
+            tokenCalculator = tokenCalculator! + 2
+        case 45:
+            tokenCalculator = tokenCalculator! + 3
+        case 60:
+            tokenCalculator = tokenCalculator! + 4
+        default:
+            break
+        }
+        
+        mealTokens.text = String(tokenCalculator! / 3)
+        snackTokens.text = String(tokenCalculator! % 3)
+
+    }
+    
+    @IBAction func workout60Button(sender: UIButton)
+    {
+        var digit: Int = sender.currentTitle!.toInt()!
+        
+        switch digit
+        {
+        case 15:
+            tokenCalculator = tokenCalculator! + 1
+        case 30:
+            tokenCalculator = tokenCalculator! + 2
+        case 45:
+            tokenCalculator = tokenCalculator! + 3
+        case 60:
+            tokenCalculator = tokenCalculator! + 4
+        default:
+            break
+        }
+        
+        mealTokens.text = String(tokenCalculator! / 3)
+        snackTokens.text = String(tokenCalculator! % 3)
+
+    }
+    
+    @IBAction func eatSnackButton(sender: UIButton)
+    {
+        tokenCalculator = tokenCalculator! - 1
+        mealTokens.text = String(tokenCalculator! / 3)
+        snackTokens.text = String(tokenCalculator! % 3)
+    }
+    
+    @IBAction func eatMealButton(sender: UIButton)
+    {
+        tokenCalculator = tokenCalculator! - 3
+        mealTokens.text = String(tokenCalculator! / 3)
+        snackTokens.text = String(tokenCalculator! % 3)
+    }
+    
+    
+    
+    
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()

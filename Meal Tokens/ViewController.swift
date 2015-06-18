b54e98a840a51725a589a7bba52cde3cd1385974
IssacGreenfield <issacgreenfield@gmail.com>
2015-06-18 11:25:38 -0700

    warning on 15 min working

--- Meal Tokens/ViewController.swift
+++ Meal Tokens/ViewController.swift
@@ -22,23 +22,35 @@ class ViewController: UIViewController {
     {
         var digit: Int = sender.currentTitle!.toInt()!
 
-        switch digit
-        {
-        case 15:
-            tokenCalculator = tokenCalculator! + 1
-        case 30:
-            tokenCalculator = tokenCalculator! + 2
-        case 45:
-            tokenCalculator = tokenCalculator! + 3
-        case 60:
-            tokenCalculator = tokenCalculator! + 4
-        default:
-            break
-        }
+        var refreshAlert = UIAlertController(title: "Hold up", message: "Did you really work out for 15 minutes?", preferredStyle: UIAlertControllerStyle.Alert)
         
-        mealTokens.text = String(tokenCalculator! / 3)
-        snackTokens.text = String(tokenCalculator! % 3)
+        refreshAlert.addAction(UIAlertAction(title: "I did!", style: .Default, handler: { (action: UIAlertAction!) in
+            //println("Handle Ok logic here")
+            switch digit
+            {
+            case 15:
+                self.tokenCalculator = self.tokenCalculator! + 1
+            case 30:
+                self.tokenCalculator = self.tokenCalculator! + 2
+            case 45:
+                self.tokenCalculator = self.tokenCalculator! + 3
+            case 60:
+                self.tokenCalculator = self.tokenCalculator! + 4
+            default:
+                break
+            }
+            self.mealTokens.text = String(self.tokenCalculator! / 4)
+            self.snackTokens.text = String(self.tokenCalculator! % 4)
+        }))
+        
+        refreshAlert.addAction(UIAlertAction(title: "Blch... no", style: .Default, handler: { (action: UIAlertAction!) in
+            //println("Handle Cancel Logic here")
+        }))
+        
+        presentViewController(refreshAlert, animated: true, completion: nil)
+        
     }
+    
     @IBAction func workout30Button(sender: UIButton)
     {
         var digit: Int = sender.currentTitle!.toInt()!
@@ -57,8 +69,8 @@ class ViewController: UIViewController {
             break
         }
         
-        mealTokens.text = String(tokenCalculator! / 3)
-        snackTokens.text = String(tokenCalculator! % 3)
+        mealTokens.text = String(tokenCalculator! / 4)
+        snackTokens.text = String(tokenCalculator! % 4)
 
     }
     
@@ -80,8 +92,8 @@ class ViewController: UIViewController {
             break
         }
         
-        mealTokens.text = String(tokenCalculator! / 3)
-        snackTokens.text = String(tokenCalculator! % 3)
+        mealTokens.text = String(tokenCalculator! / 4)
+        snackTokens.text = String(tokenCalculator! % 4)
 
     }
     
@@ -103,23 +115,29 @@ class ViewController: UIViewController {
             break
         }
         
-        mealTokens.text = String(tokenCalculator! / 3)
-        snackTokens.text = String(tokenCalculator! % 3)
+        mealTokens.text = String(tokenCalculator! / 4)
+        snackTokens.text = String(tokenCalculator! % 4)
 
     }
     
     @IBAction func eatSnackButton(sender: UIButton)
     {
+        if tokenCalculator! > 0
+        {
         tokenCalculator = tokenCalculator! - 1
-        mealTokens.text = String(tokenCalculator! / 3)
-        snackTokens.text = String(tokenCalculator! % 3)
+        mealTokens.text = String(tokenCalculator! / 4)
+        snackTokens.text = String(tokenCalculator! % 4)
+        }
     }
     
     @IBAction func eatMealButton(sender: UIButton)
     {
-        tokenCalculator = tokenCalculator! - 3
-        mealTokens.text = String(tokenCalculator! / 3)
-        snackTokens.text = String(tokenCalculator! % 3)
+        if tokenCalculator! >= 4
+        {
+        tokenCalculator! = tokenCalculator! - 4
+        mealTokens.text = String(tokenCalculator! / 4)
+        snackTokens.text = String(tokenCalculator! % 4)
+        }
     }
     
     
